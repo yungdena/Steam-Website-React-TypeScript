c115497fe26ca6aca6453d5ed015cfcd887dd503
yungdena <yungdena54@gmail.com>
2023-09-05 21:46:06 +0300

    url

--- packages/frontend/src/modules/common/services/base-url.js
+++ packages/frontend/src/modules/common/services/base-url.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://steam-api.onrender.com/api";
-export const BASE_URL = "http://localhost:4200/api";
+export const BASE_URL = "https://steam-api.onrender.com/api";
+// export const BASE_URL = "http://localhost:4200/api";
