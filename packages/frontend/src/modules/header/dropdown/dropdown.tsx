e91143652340c454f9ccf8baf67dbadb4609f13f
yungdena <yungdena54@gmail.com>
2023-09-18 11:47:08 +0300

    fixed log out

--- packages/frontend/src/modules/header/dropdown/dropdown.tsx
+++ packages/frontend/src/modules/header/dropdown/dropdown.tsx
@@ -1,9 +1,11 @@
 import { useHistory } from "react-router-dom";
+import { useUserData } from "../../common/context/user-context";
 
 import { DropdownContainer, DropdownLink } from "./dropdown.styled";
 
 export const Dropdown = ({ linkId, hoveredLink, content }: { linkId: string, hoveredLink: string | null, content: {title: string, linkTo: string}[] | undefined }) => {
   const history = useHistory()
+  const UserDataContext = useUserData();
   const isHovered = hoveredLink === linkId;
 
   if (!isHovered) {
@@ -14,11 +16,14 @@ export const Dropdown = ({ linkId, hoveredLink, content }: { linkId: string, hov
     history.push(route);
   }
 
+
   const handleLogOut = (route: string) => {
+    UserDataContext?.setUser(null);
+
     localStorage.removeItem("account");
     sessionStorage.removeItem("account");
+
     history.push(route);
-    window.location.reload();
   };
 
     return (
